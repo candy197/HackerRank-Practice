669135cc72beaa251930ec34723f8c203c287494
candy197 <candy.jacob1997@gmail.com>
2023-12-12 04:46:12 +0530

    Updated

--- Python/ifelse_Problem.py
+++ Python/ifelse_Problem.py
@@ -9,19 +9,22 @@ import sys
 if __name__ == '__main__':
     n = int(input().strip())
 
-if n%2==1:
-    if n in range(2,5):
-        if n%2==0:
-            print("Not Weird")
+
+if n in range(2,5):
+    if n%2==0:
+        print("Not Weird")
             
-        else:
-            print("Weird")
-    elif (n in range(6,20)):
-        if n%2==0:
-            print("Weird")
+    else:
+        print("Weird")
+elif (n in range(6,20)):
+    if n%2==0:
+        print("Weird")
             
-        else:
-            print("Not Weird")
-    elif (n > 20):
-        if n%2==0:
-            print("Not Weird")
+    else:
+        print("Not Weird")
+elif (n > 20):
+    if n%2==0:
+        print("Not Weird")
+    else :
+        print("Weird")
+       
