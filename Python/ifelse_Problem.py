2615939d096a8dc6cf8769f9f2e23de532e9bbc7
candy197 <candy.jacob1997@gmail.com>
2023-12-12 04:55:08 +0530

    updated

--- Python/ifelse_Problem.py
+++ Python/ifelse_Problem.py
@@ -4,7 +4,12 @@ import random
 import re
 import sys
 
-
+"""
+1 codition Odd number should be print in weird
+2 even number inclusive(in range) range of 2 to 5 not weird
+3 even number inclusive 6 to 20 print weird
+4 even and greater than 20 print not weird
+""""
 
 if __name__ == '__main__':
     n = int(input().strip())
