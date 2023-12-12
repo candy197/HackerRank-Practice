930a5942368b70d9f89ff0bac211e5a2eb46600d
candy197 <candy.jacob1997@gmail.com>
2023-12-12 18:50:13 +0530

    updated

--- Python/ifelse_Problem.py
+++ Python/ifelse_Problem.py
@@ -9,7 +9,7 @@ import sys
 2 even number inclusive(in range) range of 2 to 5 not weird
 3 even number inclusive 6 to 20 print weird
 4 even and greater than 20 print not weird
-""""
+"""
 
 if __name__ == '__main__':
     n = int(input().strip())
